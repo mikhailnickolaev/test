46515afd944548eec0c292b56f3e6ae2b2908216
Mikhail <mikhail@readymag.com>
2018-11-25 22:09:34 +0300

    Задание 1

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
-console.log (leftBoredwidth);
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("ВВведите дату в формате YYYY-MM-DD");
 
-var leftBoredwidth = 1;
-let second = 2;
-const pi = 3.14;
+
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+};
+
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+
+
+alert(appData.budget / 30);
